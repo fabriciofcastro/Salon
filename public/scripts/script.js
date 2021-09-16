8d188766bf829436563ffcbf983b22a7311c066e
fabriciofcastro <fabriciofeitosacastro@gmail.com>
2021-09-16 18:46:39 -0300

    criado script de  avento scroll para header

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,1 +1,12 @@
 
+const header = document.querySelector('#header')
+const headerHeight = header.offsetHeight
+
+window.addEventListener('scroll', () => {
+    if( window.scrollY >= headerHeight) {
+        header.classList.add('scroll-header')
+    }
+    else{
+        header.classList.remove('scroll-header')
+    }
+})
